e55a9083b197e5d5cb4e5bf79624b78db4ef1f73
Kaitlin Peck <kaitpeck@live.unc.edu>
2023-01-24 21:40:02 -0500

    Update on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -9,7 +9,7 @@ if (len(five_char_word) != 5):
     exit()
 else:
     single_char: str = input("Enter a single character: ")
-    if (len(single_char)!= 1):
+    if (len(single_char) != 1):
         print("Error: Character must be a single character.")
         exit()
     else: 
@@ -18,35 +18,38 @@ else:
         if (five_char_word[0] == single_char):
             print(single_char + " found at index 0")
             instances: int = 1
-        else: instances: int = 0
+        else: 
+            instances: int = 0
 
         if (five_char_word[1] == single_char):
             print(single_char + " found at index 1")
             instances = instances + 1
-        else: instances = instances + 0
+        else: 
+            instances = instances + 0
 
         if (five_char_word[2] == single_char):
             print(single_char + " found at index 2")
             instances = instances + 1
-        else: instances = instances + 0
+        else: 
+            instances = instances + 0
 
         if (five_char_word[3] == single_char):
             print(single_char + " found at index 3")
             instances = instances + 1
-        else: instances = instances + 0
+        else: 
+            instances = instances + 0
 
         if (five_char_word[4] == single_char):
             print(single_char + " found at index 4")
             instances = instances + 1
-        else: instances = instances + 0
+        else: 
+            instances = instances + 0
 
         if (instances == 0):
-            print("No instances of " + single_char + " found in " + five_char_word )
+            print("No instances of " + single_char + " found in " + five_char_word)
         else:
             if (instances == 1):
                 print(str(instances) + " instance of " + single_char + " found in " + five_char_word)
             else: 
                 if (instances > 1):
                     print(str(instances) + " instances of " + single_char + " found in " + five_char_word)
-
-
